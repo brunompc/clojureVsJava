3efa2af6b5357f9622a3d927e29c8d06e3f28722
Bruno Cipriano <bcipriano@gmail.com>
2021-12-09 23:07:26 +0000

    New example: creating a new JFrame from Clojure

--- my-stuff/my_stuff/core.clj
+++ my-stuff/my_stuff/core.clj
@@ -1,8 +1,12 @@
 (ns my-stuff.core
-  (:gen-class))
+  (:gen-class)
+  (:import (javax.swing JFrame JLabel))
+  )
 
 ;(import '[src.java.pastrami.Animal])
 ;(import '[Animal])
+
+
 
 (defn brown-sugar []
   (let [obj (new src.java.pastrami.Animal "asdrubal")]
@@ -16,11 +20,20 @@
     (println (str "Nome depois: " (.getName obj)))
     ))
 
+(defn show-window []
+  (let [f (JFrame. "YOLO"),
+        label (new JLabel "Pastrami")]
+    (.setBounds f 300 300 300 300)
+    (.setVisible f true)
+    (.add f label)
+    ))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
   (println (brown-sugar))
   (print "this is the end")
+  (show-window)
   )
 
